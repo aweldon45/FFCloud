b868f6c83668d04befbeaf479914a0886e4746c8
Tony <anthony.weldon.jr@gmail.com>
2018-10-27 14:32:27 -0400

    dynamic data render for cards

--- client/src/components/Home.vue
+++ client/src/components/Home.vue
@@ -1,34 +1,46 @@
- <template>
+<template>
   <div>
     <div :style="BackgroundStyling">
       <div :style="mainBannerStyling">
-        <h2>{{message}}</h2>
+        <h2 v-text="message"></h2>
       </div>
   </div>
   <br>
   <ul>
-  <li :style='cardStyling'>
+   <li :style='cardStyling'>
     <div :style='cardImgStyling'>
-      Image
+      <a href='#'>
+      <img :src='filmImage' :style='Imgstyling'>
+      </a>
     </div>
     <div :style='cardTxtStyling'>
-      <h4>Holly's film</h4>
+      <h4 v-html='filmTitle'></h4>
+      <h5 v-html='filmDirector'></h5>
+      <h5 v-html='filmFestival'></h5>
     </div>
   </li>
   <li :style='cardStyling'>
     <div :style='cardImgStyling'>
-      Image
+      <a href='#'>
+      <img src='https://i.imgur.com/VhJTQlU.jpg' :style='Imgstyling'>
+      </a>
     </div>
     <div :style='cardTxtStyling'>
       <h4>Jeffrey's film</h4>
+      <h5>Jeffrey Film Festival</h5>
+      <h5>Oct 27, 2018 10:00PM</h5>
     </div>
   </li>
   <li :style='cardStyling'>
     <div :style='cardImgStyling'>
-      Image
+      <a href='#'>
+      <img src='https://i.imgur.com/P4172CV.jpg' :style='Imgstyling'>
+      </a>
     </div>
     <div :style='cardTxtStyling'>
       <h4>Tony's film</h4>
+      <h5>Aubrey Film Festival</h5>
+      <h5>Oct 28, 2018 12:00PM</h5>
     </div>
   </li>
 </ul>
@@ -37,18 +49,41 @@
 
 <script>
 
-/* const filmList = {
-  Img: '',
-  Title: 'Hollys Film',
+const filmList = [{
+  Title: "Holly's Film",
   Festival: 'Encore Film Festival',
-  Director: 'Holly Weldon'
-} */
+  Director: 'Holly Weldon',
+  Img: 'https://i.imgur.com/SxhtThr.jpg'
+},
+{
+  Title: "Jeffrey's Film",
+  Festival: 'Jeffrey Film Festival',
+  Director: 'Jeffrey Weldon',
+  Img: 'https://i.imgur.com/VhJTQlU.jpg'
+}
+]
+
+const filmReturn = (name, prop) => {
+  for (var i = 0; i < filmList.length; i++) {
+    if (filmList[i].Title === name) {
+      return filmList[i][prop]
+    } else {
+      return 'invalid entry'
+    }
+  }
+}
 
 export default {
   data () {
     return {
       message: 'Experience your favorite festivals from anywhere!',
-      message2: 'Simultaneous broadcast with live screenings'
+      message2: 'Simultaneous broadcast with live screenings',
+      Film1: "Holly's Film",
+      Film2: "Jeffrey's Film",
+      filmTitle: filmReturn("Holly's Film", 'Title'),
+      filmImage: filmReturn("Holly's Film", 'Img'),
+      filmDirector: filmReturn("Holly's Film", 'Director'),
+      filmFestival: filmReturn("Holly's Film", 'Festival')
     }
   },
   computed: {
@@ -96,8 +131,7 @@ export default {
         boxSizing: 'border-box',
         width: '100%',
         height: '200px',
-        top: '0',
-        backgroundColor: '#F39C12'
+        top: '0'
       }
     },
     cardTxtStyling: function () {
@@ -108,6 +142,12 @@ export default {
         height: '150px',
         bottom: '0',
         backgroundColor: 'white'
+      }
+    },
+    Imgstyling: function () {
+      return {
+        width: '100%',
+        height: '100%'
       }
     }
   }
